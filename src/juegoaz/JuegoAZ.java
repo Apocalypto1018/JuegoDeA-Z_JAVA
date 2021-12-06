ed43547f063e5dd45585355573210b61f8798d22
Apocalypto1018 <ricardo.medina.d123@gmail.com>
2021-12-06 16:01:28 -0400

    mejoras

--- src/juegoaz/JuegoAZ.java
+++ src/juegoaz/JuegoAZ.java
@@ -9,7 +9,8 @@ import java.util.Scanner;
  * @author 
  */
 public class JuegoAZ {
-
+    private static char[] letra=new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'};
+    
     private static LinkedList<Jugador> jugadores=new LinkedList<Jugador>();
     private static Scanner sc = new Scanner(System.in);
     
@@ -33,7 +34,8 @@ public class JuegoAZ {
          do {
             //menu principal
             System.out.print("   Bienvenido al Juego del A a la Z\n\n");
-            System.out.print("*Ingrese 1 para registrar jugadores\n");
+            System.out.print("*Ingrese 1 para registrar jugadores y jugar\n");
+            System.out.print("*Ingrese 2 para ir al Menu de jugadores\n");
             System.out.print("*Ingrese 0 para salir\n->");
             opc=sc.nextInt();
             
@@ -49,6 +51,10 @@ public class JuegoAZ {
                 case 1:{
                     registro();
                     break;
+                }
+                
+                case 2:{
+                    menuJugadores();
                 }
                 
                 default:{
@@ -146,17 +152,299 @@ public class JuegoAZ {
          
        if(iniciar==1){
        //al confirmar la partida, se pasan los valores a la lista
-            jugadores.add(new Jugador(jugador1, 'j'));
-            jugadores.add(new Jugador(jugador2, 'j'));
-            jugadores.add(new Jugador(jugador3, 'a'));
+            jugadores.add(new Jugador(jugador1, 'j',0,0));
+            jugadores.add(new Jugador(jugador2, 'j',0,0));
+            jugadores.add(new Jugador(jugador3, 'a',0,0));
             
-            juego partida=new juego(jugador1, jugador2, jugador3, tiempo, turno); //invocacion del metodo jugar
-            partida.run();
+            run(jugador1, jugador2, jugador3, tiempo, turno); //metodo que corre el juego en si
        }else{
            System.out.print("Volviendo al menu principal\n"); //mensaje de volviendo al menu principal...
        }
        
+    }
+    
+    //metodo que corre el juego
+    public static void run(String jugador1, String jugador2, String jugador3, int tiempo, int turno){
+        
+        int iterador=0;
+        int i=0;
+        String palabra=" "; 
+    
+        int puntosJugador1=0;
+        int puntosJugador2=0;
+        
+        int aciertos1=0;
+        int aciertos2=0;
+        
+        int fallos1=0;
+        int fallos2=0;
+        
+        i=turno;
+        
+        while(!palabra.equals("fin")){ //ciclo que sirve como loop del juego
+            
+            if(i==0){
+                
+                marcador(jugador1, tiempo, iterador, puntosJugador1); 
+                
+                System.out.print("\n*Ingrese la palabra -> ");
+                palabra=sc.nextLine();
+                
+                if(!palabra.equals("fin")){
+                    
+                    if(validarPalabra(palabra, jugador3)){
+                        System.out.print("\n*Correcto. Ha ganado un punto extra\n");
+                        puntosJugador1++;
+                        aciertos1++;
+                    }else{
+                        System.out.print("\n*Incorrecto. Perdera un punto como penalizacion\n");
+                        puntosJugador1--;
+                        fallos1++;
+                    }
+                
+                    i=1; //indica que el siguiente turno sera para el otro jugador
+                }
+    
+            }else{
+                
+                marcador(jugador2, tiempo, iterador, puntosJugador2);
+                
+                System.out.print("\n*Ingrese la palabra -> ");
+                palabra=sc.nextLine();
+                
+                if(!palabra.equals("fin")){
 
+                    if(validarPalabra(palabra, jugador3)){
+                        System.out.print("\n*Correcto. Ha ganado un punto extra\n");
+                        puntosJugador2++;
+                        aciertos2++;
+                    }else if(!palabra.equals("fin")){
+                        System.out.print("\n*Incorrecto. Perdera un punto como penalizacion\n");
+                        puntosJugador2--;
+                        fallos2++;
+                    }
+
+                    i=0; //indica que el siguiente turno sera para el otro jugador
+                }   
+                
+            }
+            
+            iterador++;  
+            
+            System.out.print("\nEsperando enter...");
+            sc.nextLine(); //se limpia el buffer
+            
+            if(iterador>26){ //si el iterador sobrepasa la cantidad de letras del abecedario
+                iterador=0; //vuelve a acero
+            }
+            
+        }
+ 
+        int itera = 0;
+        int captura = -1;
+        for (Jugador actual : jugadores) {
+            if (actual.getNombre().equals(jugador1)) {
+                captura = itera;
+            }
+            itera++;
+        }
+        
+        jugadores.get(captura).setAciertos(aciertos1);
+        jugadores.get(captura).setFallos(fallos1);
+        
+        itera = 0;
+        captura = -1;
+        
+        for (Jugador actual : jugadores) {
+            if (actual.getNombre().equals(jugador2)) {
+                captura = itera;
+            }
+            itera++;
+        }
+        
+        jugadores.get(captura).setAciertos(aciertos2);
+        jugadores.get(captura).setFallos(fallos2);
+        
+        System.out.print("Volviendo al menu principal\n\n");
+        
+    }
+    
+    public static void marcador(String jugador, int tiempo, int iterador, int puntos){
+        System.out.print("\n\n        Jugando de la A a la Z!!\n\n");
+     
+        System.out.print("Turno del jugador: "+jugador);
+        
+        System.out.print("    Letra: "+letra[iterador]);
+        
+        System.out.print("    Puntos: "+puntos);
+   
+    }
+    
+    public static boolean validarPalabra(String palabra, String arbitro){
+        char orden;
+        
+        //validar elegir orden
+        do {
+            System.out.print(arbitro+" ingrese 's' si la palabra: "+palabra+" es acertada, o 'n' de lo contrario\n->");
+            orden= sc.next().charAt(0); //se lee el caracter     
+
+            if (orden != 's' && orden != 'n') {
+              System.out.println("ERROR!, debe introducir el caracter s, o el caracter n para validar la palabra");
+            }
+        } while (orden != 's' && orden != 'n'); //ciclo que valida el ingreso de un caracter correcto
+        
+        if(orden=='s'){
+            return true;
+        }else{
+            return false;
+        }  
+    }
+    
+    //menu de jugadores
+    public static void menuJugadores(){
+        String id;
+        String nombre, tipo;
+        char respuesta = ' ';
+        int opcionCliente;
+        do {
+            System.out.print("1. Mostrar Lista de Jugadores.\n2. Eliminar Jugador por ID. \n3. Modificar Jugador."
+                    + "\n0. Regrasar al menu principa.\n->");
+            opcionCliente = sc.nextInt();
+            switch (opcionCliente) {
+                case 0: {
+                    System.out.println("Volviendo al menu Principal");
+                    break;
+                }
+                case 1:{
+                        System.out.println("             Lista de Jugadores Totales.");
+                     for (Jugador actual : jugadores) {
+                        
+                        System.out.println("\n\n*ID: " + actual.getID());
+                        System.out.println("*Nombre: " + actual.getNombre());
+                        if(actual.getTipo()=='j'){
+                            System.out.println("*Tipo: Jugador" );
+                            System.out.println("*Aciertos: " + actual.getAciertos());
+                            System.out.println("*Fallos: " + actual.getFallos());
+                        }else{
+                           System.out.println("*Tipo: Arbitro" ); 
+                        }
+                        
+
+                    }
+        
+                    System.out.print("\n\n");
+                    break;
+                }
+                   
+                case 2:{
+                    System.out.println("            Eliminar Jugador.");
+                    System.out.print("\n*Ingrese el ID del jugador a aliminar\n->");
+                    id = sc.nextLine();
+                    sc.nextLine();
+
+                    int itera = 0;
+                        int captura = -1;
+                        for (Jugador actual : jugadores) {
+                            if (actual.getID().equals(id)) {
+                                captura = itera;
+                            }
+                            itera++;
+                        }
+                        if (captura != -1) {
+                            System.out.println("\n\n*ID: " + jugadores.get(captura).getID());
+                            System.out.println("*Nombre: " + jugadores.get(captura).getNombre());
+                            System.out.println("*Tipo: " + jugadores.get(captura).getTipo());
+                            System.out.println("*Aciertos: " + jugadores.get(captura).getAciertos());
+                            System.out.println("*Fallos: " + jugadores.get(captura).getFallos());
+                            System.out.println("Desea eliminar a este Jugador? s/n");
+                            respuesta = sc.next().charAt(0);
+                            if (respuesta == 's') {
+                                jugadores.remove(captura);
+                                System.out.println("Colaborador eliminado exitosamente");
+                            } else {
+                                break;
+                            }
+                        } else {
+                            System.out.println("No existen datos para el ID ingresado");
+                        }
+                    
+                    break;
+                }
+                
+                case 3:{
+                    sc.nextLine();
+                    System.out.println("Modificar Jugador");
+                    System.out.println("Ingrese ID del Jugador");
+                    id = sc.nextLine();
+                     
+                        int itera = 0;
+                        int captura = -1;
+                        for (Jugador actual : jugadores) {
+                            if (actual.getID().equals(id)) {
+                                captura = itera;
+                            }
+                            itera++;
+                        }
+                        if (captura != -1) {
+                            modificaJugador(captura);
+                        } else {
+                            System.out.println("No existen datos para el ID ingresado");
+                        }
+                    
+                    break;
+                }
+
+                default:
+                    System.out.println("Opcion no valida");
+
+            }
+        } while (opcionCliente != 0);
+    }
+    
+    //sub menu para modificar los jugadores
+    public static void modificaJugador(int index) {
+        int opcionModifica = -1;
+        String nombre;
+        char tipo;
+        do {
+            System.out.print("1.Nombre.\n2.Tipo.\n3.Nuevo ID aleatorio"
+                    + "\n0. volver.\n->");
+            opcionModifica = sc.nextInt();
+            sc.nextLine();
+            switch (opcionModifica) {
+                case 0:
+                    System.out.println("*Volviendo al menu anterior");
+                    break;
+                case 1:
+                    System.out.println("*Nuevo Nombre:");
+                    nombre = sc.nextLine();
+                    jugadores.get(index).setNombre(nombre);
+                    break;
+                case 2:
+        
+                    do {
+                        System.out.println("*Nuevo Tipo: a= arbitro / j= jugador");
+                        tipo = sc.next().charAt(0);
+
+                        if (tipo != 'a' && tipo != 'j') {
+                          System.out.println("ERROR!, debe introducir o el caracter a, o el caracter j para indicar tipo de jugador");
+                        }
+                    } while (orden != 'a' && orden != 'j'); //ciclo que valida el ingreso de un caracter correcto
+                    jugadores.get(index).setTipo(tipo);
+                    break;
+                case 3:
+                    jugadores.get(index).nuevoID();
+                    System.out.println("*Nuevo id: "+jugadores.get(index).getID());
+                
+                default:{
+                    System.out.println("Ingrese una opcion valida");
+                    break;
+                }
+                    
+                    
+
+            }
+        } while (opcionModifica != 0);
     }
     
     //metodo que elige el orden inicial de turno (genera un numero al azar entre 0 y 2 excluyendo al 2)
--- src/juegoaz/Jugador.java
+++ src/juegoaz/Jugador.java
@@ -1,38 +1,99 @@
 
 package juegoaz;
 
+import java.util.UUID;
+
 /**
  *
  * @author 
  */
-public class Jugador {
+public final class Jugador {
+    
+    private String id;
     
     private String nombre;
     private char tipo;
     
+    private int aciertos;
+    private int fallos;
+    
     //constructor
-    public Jugador(String nombre, char tipo){
+    public Jugador(String nombre, char tipo, int aciertos, int fallos){
+        this.id=generarID();
+        
         this.nombre=nombre;
         this.tipo=tipo;
+        this.aciertos=aciertos;
+        this.fallos=fallos;
+    }
+    
+    //metodo que genero un id unico
+    public String generarID(){
+            
+        UUID uniqueKey = UUID.randomUUID();
+
+        return filter(String.valueOf(uniqueKey));
+    }
+    
+    //metodo que toma los 3 primeros digitos del id generado
+    public String filter(String word) {
+
+        StringBuilder builder = new StringBuilder();
+
+        String palabras[] = word.split(" ");
+
+        for (String palabra : palabras) {
+
+            if (palabra.length() >= 3) {
+               builder.append(palabra.substring(0, 3));
+            } else {
+               builder.append(palabra);
+            }
+        }
+        return builder.toString();
     }
     
     //setters
+    public void nuevoID(){
+        this.id=generarID();
+    }
+    
     public void setNombre(String nombre){
         this.nombre=nombre;
     }
 
-    
     public void setTipo(char tipo){
         this.tipo=tipo;
     }
     
+    public void setAciertos(int aciertos){
+        this.aciertos=aciertos;
+    }
+    
+    public void setFallos(int fallos){
+        this.fallos=fallos;
+    }
+            
+    
     //getters
+    public String getID(){
+        return this.id;
+    }
+    
     public String getNombre(){
         return this.nombre;
     }
     
     public char getTipo(){
         return this.tipo;
+    }
+    
+    public int getAciertos(){
+        return this.aciertos;
+    }
+    
+    public int getFallos(){
+        return this.fallos;
     }
     
 }
